1af0ee748af91da1dfc8cde9eacca358eaa5d659
flori <floritrs@gmail.com>
2025-11-06 13:55:33 +0100

    create directory for fabric 1.21.9

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.jdk8
+            pkgs.jdk21
+            pkgs.jdk11
+            pkgs.jdk17
+            pkgs.gradle
+            pkgs.jdt-language-server
+            (pkgs.jetbrains.idea-community.override {
+              # forceWayland = true;
+              vmopts = "-Dawt.toolkit.name=WLToolkit";
+            })
+            pkgs.maven
+
+            pkgs.moreutils # chronic for ./build.sh
+          ];
+
+          buildInputs = [
+            pkgs.glfw
+            pkgs.libGL
+
+            pkgs.libpulseaudio
+            pkgs.openal
+
+            pkgs.flite
+          ];
+
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+            pkgs.glfw
+            pkgs.libGL
+
+            pkgs.libpulseaudio
+            pkgs.openal
+
+            pkgs.flite
+          ];
+
+          shellHook = ''
+            export JAVA_HOME=${pkgs.jdk8}
+            export JAVA8_HOME=${pkgs.jdk8}
+            export JAVA11_HOME=${pkgs.jdk11}
+            export JAVA17_HOME=${pkgs.jdk17}
+            export JAVA21_HOME=${pkgs.jdk21}
+          '';
+        };
+      }
+    );
+}
